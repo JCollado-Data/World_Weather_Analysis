8c9e1e6d58ffc734b3d66479c1a5d7e6a72dcf01
Javi <colladoj2089@gmail.comgit config --global user.name git config -- global user.name Javiergit commit -m updated>
2022-07-10 17:02:05 -0700

    Push project

---
+++ Vacation_Search/config.py
@@ -0,0 +1,4 @@
+weather_api_key= 'aabf72360172fedf6b087bccfba6b967'
+
+#Google API Key
+g_key = 'AIzaSyBYaMi-Omvvpczm6rT8uyHgj_429KUEoY8'
---
+++ Weather_Database/config.py
@@ -0,0 +1,4 @@
+weather_api_key= 'aabf72360172fedf6b087bccfba6b967'
+
+#Google API Key
+g_key = 'AIzaSyDB7sSl6cyQtIpE31XNhnfzJqRDP7gAr7A'
